34f658ec2c497c20483683713f407ee96c266964
ShutenkoStas <shotenko494@gmail.com>
2025-12-07 00:32:58 +0200

    Add new url and templates for login page

--- reservation/urls.py
+++ reservation/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from reservation import views
 
 urlpatterns = [
@@ -6,6 +6,7 @@ urlpatterns = [
     path("rooms_list", views.room_list, name="room_list"),
     path("book_room", views.book_room, name="book_room"),
     path("booking_details/<int:pk>/", views.booking_details, name="booking_details"),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
 
 
