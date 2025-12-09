6bfb8bc195d3a6a314674e0cf59b1e9e410a2b2b
ShutenkoStas <shotenko494@gmail.com>
2025-12-09 23:05:07 +0200

    Complete first varaiand for registration add view add url make starter template

--- auth_system/views.py
+++ auth_system/views.py
@@ -6,17 +6,24 @@ from django.contrib.auth import login, authenticate
 
 def register(request):
     if request.method == "POST":
-        pass
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("index")
     else:
         form = UserCreationForm()
 
 
     return render(
         request,
-        template_name="register.html"
+        template_name="auth_system/register.html",
+        context= {'form': form},
     )
 
 
+# def login(request):
+#     if req
 
 
 # Create your views here.
