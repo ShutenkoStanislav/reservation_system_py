bc77437e56458250a7e35b1fbbc14225e5dbf4ac
ShutenkoStas <shotenko494@gmail.com>
2025-12-09 23:20:51 +0200

    add and comment base view for login

--- auth_system/views.py
+++ auth_system/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, authenticate
+from django.contrib import messages
 
 
 
@@ -23,7 +24,25 @@ def register(request):
 
 
 # def login(request):
-#     if req
+#      if request.method == "POST":
+#         form = AuthenticationForm(
+#             request,
+#             data=request.POST,      
+#             )
+#         if form.is_valid():
+#             username = form.cleaned_data.get("username")
+#             password = form.cleaned_data.get("password")
+#             user = authenticate(request,
+#                         username=username,
+#                         password=password
+#                         )
+#             if user is not None:
+#                 login(request, user)
+#                 return redirect('index')
+#             else:
+#                 messages.error(request, "Incorrect login or password")
+
+            
 
 
 # Create your views here.
