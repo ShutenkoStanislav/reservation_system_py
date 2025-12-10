8fc8b91be2a461f1ac259c1bbe0378f32d3f37fe
ShutenkoStas <shotenko494@gmail.com>
2025-12-10 22:42:14 +0200

    Add custmon form add this form for view

--- auth_system/views.py
+++ auth_system/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from auth_system.form import CustomUserCreationForm
 from django.contrib.auth import login, authenticate
 from django.contrib import messages
 
@@ -7,7 +7,7 @@ from django.contrib import messages
 
 def register(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
@@ -15,7 +15,7 @@ def register(request):
        
             
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
         messages.error(request, "some error")
 
 
