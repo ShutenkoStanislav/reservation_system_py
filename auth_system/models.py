b7b5a9deb92a0d53f2552614ec1aae2d7978ce2c
ShutenkoStas <shotenko494@gmail.com>
2025-12-16 22:51:06 +0200

    Add more cool desing for home page and add for Custor_user middle_name

--- auth_system/models.py
+++ auth_system/models.py
@@ -3,9 +3,10 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     phone_number = models.CharField(max_length=20, blank=True, null=True)
+    middle_name = models.CharField(max_length=128, blank=True, null=True)
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}: {self.username}"
+        return f"{self.first_name} {self.middle_name} {self.last_name}: {self.username}"
     
 
 
